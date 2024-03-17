a669d469bc343610a639ca080c3168014bddf8f8
Roy SALIBA <134060194+Royalcoco@users.noreply.github.com>
2024-03-17 16:46:46 +0100

    Add files via upload

---
+++ ASPIRINE/TRY_CONVERT ; DROP SCHEMA IF EXISTS .sql
@@ -0,0 +1,26 @@
+TRY_CONVERT ; DROP SCHEMA IF EXISTS .CASCADE ;
+REVOKE INPUT SCHEMABINDING FOR SELECT
+FROM .CASCADE ; '(fun.c$tko)' ouverture delete from select loop
+    int
+end loop
+  update table
+        set field=value
+    where condition elsif raise_application_error(-20000, 'message'); then function name(
+      param in param_type
+    ) return return_type;
+      
+  from table
+where condition raise_application_error for if condition then case 
+  when condition then dbms_output.put_line('message');
+  else
+end;
+case 
+  when condition then
+  else 
+end;
+end if; in  loop
+  c$tko
+end loop
+where condition
+when value then delete from DROP
+  where condition 
